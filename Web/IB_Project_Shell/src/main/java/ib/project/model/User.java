60ce2209c1a38af627d0ec0779975a67d9129e8c
Stevan Julinac <stevanjulinac47@gmail.com>
2019-05-27 22:52:33 +0200

    Klasa User

--- Web/IB_Project_Shell/src/main/java/ib/project/model/User.java
+++ Web/IB_Project_Shell/src/main/java/ib/project/model/User.java
@@ -2,4 +2,75 @@ package ib.project.model;
 
 public class User {
 
+	private Integer id;
+	private String email;
+	private String password;
+	private String certificate;
+	private boolean active;
+	private Authority authority;
+	
+	public User() {
+		super();
+	}
+
+	public User(Integer id, String email, String password, String certificate, boolean active, Authority authority) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.certificate = certificate;
+		this.active = active;
+		this.authority = authority;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCertificate() {
+		return certificate;
+	}
+
+	public void setCertificate(String certificate) {
+		this.certificate = certificate;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Authority getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(Authority authority) {
+		this.authority = authority;
+	}
+	
+	
+	
 }
